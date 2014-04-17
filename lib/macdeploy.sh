0069108f78d63c949bfafc350ec7249df5e4b17f
Sergio Borghese <s.borghese@netresults.it>
2014-04-17 16:10:52 +0000

    fixed mac deploy script to match new paths and lib names
    git-svn-id: svn://192.168.23.200/Util/uniqlogger/trunk@85 570bcd0d-3e93-4b67-98fc-c1c68feedceb

--- lib/macdeploy.sh
+++ lib/macdeploy.sh
@@ -5,9 +5,17 @@ set -e
 QT_SYSTEM_PATH="/usr/local/Trolltech/Qt-4.8.1/lib"
 DEPLOY_DIR="deployable_binaries"
 
-install_name_tool -change ${QT_SYSTEM_PATH}/libQtCore.4.dylib @executable_path/../Frameworks/libQtCore.4.dylib bin/libUniqLogger.0.1.3.dylib
-install_name_tool -change ${QT_SYSTEM_PATH}/libQtNetwork.4.dylib @executable_path/../Frameworks/libQtNetwork.4.dylib bin/libUniqLogger.0.1.3.dylib
+if [ -e release/bin/libUniqLogger.0.2.0.dylib ] ; then
+	install_name_tool -change ${QT_SYSTEM_PATH}/libQtCore.4.dylib @executable_path/../Frameworks/libQtCore.4.dylib release/bin/libUniqLogger.0.2.0.dylib
+	install_name_tool -change ${QT_SYSTEM_PATH}/libQtNetwork.4.dylib @executable_path/../Frameworks/libQtNetwork.4.dylib release/bin/libUniqLogger.0.2.0.dylib
+fi
+
+if [ -e debug/bin/libUniqLogger_debug.0.2.0.dylib ] ; then
+	install_name_tool -change ${QT_SYSTEM_PATH}/libQtCore.4.dylib @executable_path/../Frameworks/libQtCore.4.dylib debug/bin/libUniqLogger_debug.0.2.0.dylib
+	install_name_tool -change ${QT_SYSTEM_PATH}/libQtNetwork.4.dylib @executable_path/../Frameworks/libQtNetwork.4.dylib debug/bin/libUniqLogger_debug.0.2.0.dylib
+fi
 
 [ ! -d ${DEPLOY_DIR} ] && mkdir ${DEPLOY_DIR} 
-cp -rf bin/libUniqLogger.0.dylib ${DEPLOY_DIR}/ 
+cp -rf release/bin/libUniqLogger.0.dylib ${DEPLOY_DIR}/ 
+cp -rf debug/bin/libUniqLogger_debug.0.dylib ${DEPLOY_DIR}/ 
 
