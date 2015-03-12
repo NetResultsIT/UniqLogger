134d91e05d5bb08558b30a19872023dce0ed92d1
Francesco Lamonica <f.lamonica@netresults.it>
2015-03-12 11:58:27 +0000

    BRANCH: fl-tpool * added the new cli testlogger
    git-svn-id: svn://192.168.23.200/Util/uniqlogger/branches/fl-tpool@103 570bcd0d-3e93-4b67-98fc-c1c68feedceb

---
+++ testlogger/testlogger2.cpp
@@ -0,0 +1,151 @@
+#include "testlogger2.h"
+
+#include <QDebug>
+#include <QTimer>
+
+#define TEST_FILE_ROTATION 0
+#define TEST_NET 0
+#define TEST_DB 0
+#define TEST_CONSOLE_COLOR 1
+#define TEST_MONITOR 0
+#define TEST_FORMATTING 0
+
+testlogger_cli::testlogger_cli(QObject *parent)
+    : QObject(parent)
+{
+    loggerF = NULL; //This will be a file logger
+    loggerN2 = NULL; //This will be red console
+    loggerN1 = NULL;
+    loggerCr = NULL;
+    loggerCy = NULL;
+    loggerCg = NULL;
+    loggerD = NULL;
+
+    QTimer *timer = new QTimer();
+
+    connect(timer, SIGNAL(timeout()), this, SLOT(timedLog()));
+
+    WriterConfig wc2;
+    wc2.maxFileNum = 3;
+    wc2.maxFileSize = 2;
+
+    UniqLogger *ul = UniqLogger::instance();
+
+#if(TEST_FORMATTING)
+    ul->setEncasingChars('(',')');
+    ul->setSpacingChar('.');
+    ul->setTimeStampFormat("hh.mm.ss.zzz");
+#endif
+
+    loggerF = ul->createFileLogger("test", "log.txt", wc2);
+    loggerCr = ul->createConsoleLogger("CONSOLE", red);
+    loggerCy = ul->createConsoleLogger("CONSOLE", brown);
+    loggerCg = ul->createConsoleLogger("CONSOLE", green);
+
+#if(TEST_NET)
+    WriterConfig wconf;
+    wconf.maxMessageNum = 10;
+    loggerN2 = ul->createNetworkLogger("net test2", "127.0.0.1",1675);
+    loggerN1 = ul->createNetworkLogger("net test", "127.0.0.1",1674, wconf);
+    const LogWriter &nlw = ul->getNetworkWriter("127.0.0.1",1674);
+    //ul->addWriterToLogger(logger,nlw);
+
+    //testThreadedNetLogger("127.0.0.1", 1674);
+#endif
+
+#if(TEST_DB)
+    loggerD = ul->createDbLogger("DbLogger","log.db");
+#endif
+
+
+    loggerF->setModuleName("FILE");
+    loggerCr->setModuleName("REDCONSOLE");
+    loggerCy->setModuleName("BROWNCONSOLE");
+    loggerCg->setModuleName("GREENCONSOLE");
+
+    //Start timed logging
+    timer->start(2000);
+}
+
+
+void
+testlogger_cli::testThreadedNetLogger(const QString &ip, int port)
+{
+    Q_UNUSED(ip);
+    Q_UNUSED(port);
+
+    TestThreadObject2 *tobj1 = new TestThreadObject2(2000);
+    tobj1->l = loggerN1;
+    TestThreadObject2 *tobj2 = new TestThreadObject2(3000);
+    tobj2->l = loggerN1;
+
+    QThread *t1, *t2;
+    t1 = new QThread(this);
+    t2 = new QThread(this);
+    tobj1->moveToThread(t1);
+    tobj2->moveToThread(t2);
+
+    t1->start();
+    t2->start();
+}
+
+
+void
+testlogger_cli::timedLog()
+{
+    static int i=0;
+
+#if(TEST_FILE_ROTATION)
+    qDebug() << "written " << ++i * 100 << "KB to file...";
+    loggerF->log(UNQL::LOG_INFO, ( QString("file text ") + QString::number(i) + QString().fill('a', 100000) ).toLatin1().constData() );
+#endif
+
+
+#if(TEST_CONSOLE_COLOR)
+    loggerCr->log(UNQL::LOG_CRITICAL, ( QString("console text - ") + QString::number(i) ).toLatin1().constData());
+    *loggerCy << UNQL::LOG_WARNING << QString("console text 1st part - ") + QString::number(i);
+    *loggerCy << QString("console text 2nd part - ") + QString::number(i) << UNQL::EOM;
+    *loggerCg << UNQL::LOG_INFO << ( QString("console text - ") + QString::number(i++) ).toLatin1().constData() << UNQL::EOM;
+#endif
+
+
+#if(TEST_NET)
+    loggerN2->log(UNQL::LOG_CRITICAL, (QString("net critical")+QString::number(i)).toLatin1().constData());
+    loggerN1 << UNQL::LOG_INFO << (QString("net info ")+QString::number(i++)) << UNQL::EOM;
+#endif
+
+
+#if(TEST_DB)
+        loggerD->log(UNQL::LOG_INFO, "Testing db log" + QString::number(i));
+        *loggerD << UNQL::LOG_CRITICAL << "This is a critical db error!" << QString::number(i++) << UNQL::EOM;
+        qDebug() << "Writing to db...";
+#endif
+
+
+#if(TEST_MONITOR)
+    double dd = qrand();
+
+    QString key = "groupKey";
+    loggerCr->setModuleName("Red Monitor");
+    loggerCr->monitor(dd, key, "test variable dd");
+    loggerCr->monitor(i, key, "iteration number");
+    i++;
+
+    //after 5 iteration start monitoring
+    if (i==5) {
+        loggerCr->setModuleName("Red Monitor active");
+        UniqLogger *ul = UniqLogger::instance();
+        ul->monitorVar(key, true);
+    }
+
+    //now at 10th iteration stop monitoring
+    if (i==15) {
+        loggerCr->setModuleName("Red Monitor");
+        UniqLogger *ul = UniqLogger::instance();
+        ul->monitorVar(key, false);
+    }
+#endif
+
+}
+
+
---
+++ testlogger/testlogger2.h
@@ -0,0 +1,60 @@
+#ifndef _CLI_TESTLOGGER_H_
+#define _CLI_TESTLOGGER_H_
+
+#include <QOBJECT>
+
+#include "UniqLogger.h"
+
+class TestThreadObject2 : public QObject
+{
+    Q_OBJECT
+
+    QTimer *m_timer;
+    int m_counter;
+    int m_timeout_msecs;
+
+public:
+    Logger *l;
+
+public:
+    explicit TestThreadObject2(int msecs, QObject *parent=0)
+        :QObject(parent),
+          m_timeout_msecs(msecs)
+    {
+        l=NULL;
+        m_timer = new QTimer(this);
+        m_counter = 0;
+        connect(m_timer, SIGNAL(timeout()), this, SLOT(logwthread()));
+        m_timer->start(m_timeout_msecs);
+    }
+
+public slots:
+    void logwthread() {
+        QString s;
+        s.sprintf("%p", QThread::currentThread());
+        s += " -- " + QString::number(m_counter++);
+        if (l)
+            *l << UNQL::LOG_INFO << s << Q_FUNC_INFO << UNQL::EOM;
+        else
+            qCritical() << QThread::currentThread() << Q_FUNC_INFO << " -- Cannot log on a NULL logger";
+    }
+};
+
+class testlogger_cli : public QObject
+{
+    Q_OBJECT
+    Logger  *loggerF, *loggerN2, *loggerN1, *loggerD;
+    Logger  *loggerCy, *loggerCg, *loggerCr;
+
+public:
+    testlogger_cli(QObject *parent = 0);
+    ~testlogger_cli() {}
+
+    void testThreadedNetLogger(const QString &ip, int port);
+    //void test_strangeString(UniqLogger *ul);
+
+protected slots:
+    void timedLog();
+};
+
+#endif // TESTLOGGER_H
