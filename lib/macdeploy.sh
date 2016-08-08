8ca2546788a828f6a4438a9330a8179d2e3a173b
Sergio Borghese <s.borghese@netresults.it>
2016-08-08 10:10:26 +0000

    uniqlogger [trunk] - renamed macdeploy5 in macdeploy (we do not use qt4 on macosx anymore) - fixed path for qt
    git-svn-id: svn://192.168.23.200/Util/uniqlogger/trunk@149 570bcd0d-3e93-4b67-98fc-c1c68feedceb

--- lib/macdeploy.sh
+++ lib/macdeploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-ULVER=0.2.0
+ULVER=0.3.3
+QTVER=5.6.1
 
 # Do not modify below here
 set -e
 
-QT_SYSTEM_PATH="/usr/local/Trolltech/Qt-4.8.1/lib"
+#Qt5
+QT_SYSTEM_PATH=/opt/Qt/${QTVER}/clang_64/lib
+
+SRCDIR=last_build
 DEPLOY_DIR="deployable_binaries"
 
-if [ -e release/bin/libUniqLogger.0.2.0.dylib ] ; then
-	install_name_tool -change ${QT_SYSTEM_PATH}/libQtCore.4.dylib @executable_path/../Frameworks/libQtCore.4.dylib release/bin/libUniqLogger.0.2.0.dylib
-	install_name_tool -change ${QT_SYSTEM_PATH}/libQtNetwork.4.dylib @executable_path/../Frameworks/libQtNetwork.4.dylib release/bin/libUniqLogger.0.2.0.dylib
+[ ! -d ${DEPLOY_DIR} ] && mkdir ${DEPLOY_DIR}
+rm -rf ${DEPLOY_DIR}/*
+cp -aP ${SRCDIR}/libUniqLogger.*dylib ${DEPLOY_DIR}/
+#cp -rf ${SRCDIR}/libUniqLogger_debug.0.dylib ${DEPLOY_DIR}/
+
+if [ -e ${SRCDIR}/libUniqLogger.${ULVER}.dylib ] ; then
+    install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ${DEPLOY_DIR}/libUniqLogger.${ULVER}.dylib
+    install_name_tool -change @rpath/QtNetwork.framework/Versions/5/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork ${DEPLOY_DIR}/libUniqLogger.${ULVER}.dylib
+    install_name_tool -change @rpath/QtSql.framework/Versions/5/QtSql @executable_path/../Frameworks/QtSql.framework/Versions/5/QtSql ${DEPLOY_DIR}/libUniqLogger.${ULVER}.dylib
 fi
-
-if [ -e debug/bin/libUniqLogger_debug.0.2.0.dylib ] ; then
-	install_name_tool -change ${QT_SYSTEM_PATH}/libQtCore.4.dylib @executable_path/../Frameworks/libQtCore.4.dylib debug/bin/libUniqLogger_debug.0.2.0.dylib
-	install_name_tool -change ${QT_SYSTEM_PATH}/libQtNetwork.4.dylib @executable_path/../Frameworks/libQtNetwork.4.dylib debug/bin/libUniqLogger_debug.0.2.0.dylib
-fi
-
-[ ! -d ${DEPLOY_DIR} ] && mkdir ${DEPLOY_DIR} 
-cp -rf release/bin/libUniqLogger.0.dylib ${DEPLOY_DIR}/ 
-cp -rf debug/bin/libUniqLogger_debug.0.dylib ${DEPLOY_DIR}/ 
-
